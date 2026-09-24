7cca8b9ebe2ce2279e989b1eac14545c6fc245d5
gkzero0-cmyk <gkzero0@gmail.com>
2026-09-25 08:40:44 +0900

    Use consolidated URL helpers in components/wiki-page-navigation.tsx

--- components/wiki-page-navigation.tsx
+++ components/wiki-page-navigation.tsx
@@ -1,5 +1,5 @@
 import type { NotionIndexPage } from '@/lib/notion-index'
-import { withBasePath } from '@/lib/base-path'
+import { withBasePath } from '@/lib/url-utils'
 
 function categoryTitle(title: string) {
   const value = title.toLowerCase()
