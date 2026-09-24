9f1506135d82d7abb2524e387ac89db9bfee799c
gkzero0-cmyk <gkzero0@gmail.com>
2026-09-25 06:12:43 +0900

    Use Notion title in wiki home

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import { getPageTitle } from 'notion-utils'
+
 import { NotionDocument } from '@/components/notion-document'
 import { WikiShell } from '@/components/wiki-shell'
 import { getNotionPage, notionPublicUrl, ROOT_PAGE_ID } from '@/lib/notion'
@@ -8,9 +10,14 @@ export const revalidate = 300
 export default async function HomePage() {
   const recordMap = await getNotionPage(ROOT_PAGE_ID)
   const imageManifest = readNotionAssetManifest()
+  const title = getPageTitle(recordMap) || '서버 위키'
 
   return (
-    <WikiShell sourceUrl={notionPublicUrl(ROOT_PAGE_ID)}>
+    <WikiShell
+      sourceUrl={notionPublicUrl(ROOT_PAGE_ID)}
+      title={title}
+      assetCount={Object.keys(imageManifest).length}
+    >
       <section className="document-card">
         <NotionDocument
           recordMap={recordMap}
