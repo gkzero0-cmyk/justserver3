639b932d87993c1106a2178d76db89c6ae9a174e
gkzero0-cmyk <gkzero0@gmail.com>
2026-09-25 08:27:13 +0900

    Turn home into dedicated wiki landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { getPageTitle } from 'notion-utils'
 
-import { NotionDocument } from '@/components/notion-document'
+import { StarterGuide } from '@/components/starter-guide'
 import { WikiDirectory } from '@/components/wiki-directory'
 import { WikiShell } from '@/components/wiki-shell'
 import { getNotionPage, notionPublicUrl, ROOT_PAGE_ID } from '@/lib/notion'
@@ -11,11 +11,11 @@ export default async function HomePage() {
   const recordMap = await getNotionPage(ROOT_PAGE_ID)
   const imageManifest = await readNotionAssetManifest()
   const notionIndex = await readNotionIndex()
-  const title = getPageTitle(recordMap) || '서버 위키'
+  const title = getPageTitle(recordMap) || '그냥서버 : 적자생존 공식 위키'
 
   const rootId = notionIndex.rootPageId.replaceAll('-', '')
   const directoryPages = notionIndex.pages.filter(
-    (page) => page.pageId !== rootId
+    (page) => page.pageId.replaceAll('-', '') !== rootId
   )
 
   return (
@@ -24,16 +24,14 @@ export default async function HomePage() {
       title={title}
       assetCount={Object.keys(imageManifest).length}
       pageCount={notionIndex.pages.length || 1}
-      pages={directoryPages.map(({ pageId, title }) => ({ pageId, title }))}
+      pages={directoryPages.map(({ pageId, title, searchText }) => ({
+        pageId,
+        title,
+        searchText
+      }))}
     >
+      <StarterGuide pages={directoryPages} />
       <WikiDirectory pages={directoryPages} />
-
-      <section className="document-card">
-        <NotionDocument
-          recordMap={recordMap}
-          imageManifest={imageManifest}
-        />
-      </section>
     </WikiShell>
   )
 }
