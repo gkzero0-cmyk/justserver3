3c8e1e37236da648b2b029ab053818b8e9849330
gkzero0-cmyk <gkzero0@gmail.com>
2026-09-25 08:40:37 +0900

    Use consolidated URL helpers in components/wiki-shell.tsx

--- components/wiki-shell.tsx
+++ components/wiki-shell.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useMemo, useRef, useState } from 'react'
 
-import { withBasePath } from '@/lib/base-path'
+import { withBasePath } from '@/lib/url-utils'
 
 type TocItem = {
   id: string
