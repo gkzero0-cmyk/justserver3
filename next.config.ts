55633212175076f187b266a7bf5aa1bca2feb7c1
gkzero0-cmyk <gkzero0@gmail.com>
2026-09-25 06:33:16 +0900

    Support conditional GitHub Pages export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from 'next'
 
+const isGitHubPages = process.env.GITHUB_PAGES === 'true'
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? ''
+
 const nextConfig: NextConfig = {
+  ...(isGitHubPages
+    ? {
+        output: 'export' as const,
+        trailingSlash: true,
+        basePath,
+        assetPrefix: basePath
+      }
+    : {}),
   images: {
+    unoptimized: isGitHubPages,
     remotePatterns: [
       { protocol: 'https', hostname: 'prod-files-secure.s3.us-west-2.amazonaws.com' },
       { protocol: 'https', hostname: 's3.us-west-2.amazonaws.com' },
