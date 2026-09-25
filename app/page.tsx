c53574185a2b36cfbe9a0300ec9fa4929b38c0b9
gkzero0-cmyk <gkzero0@gmail.com>
2026-09-25 21:22:47 +0900

    Add play zone to wiki home

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 
 import { StarterGuide } from '@/components/starter-guide'
+import { WikiFunZone } from '@/components/wiki-fun-zone'
 import { WikiDirectory } from '@/components/wiki-directory'
 import { WikiShell } from '@/components/wiki-shell'
 import { notionPublicUrl, ROOT_PAGE_ID } from '@/lib/notion'
@@ -101,6 +102,13 @@ export default async function HomePage() {
         dangerouslySetInnerHTML={{ __html: JSON.stringify(websiteJsonLd) }}
       />
       <StarterGuide pages={directoryPages} />
+      <WikiFunZone
+        pages={directoryPages.map((page) => ({
+          pageId: page.pageId,
+          title: page.title,
+          status: wikiContentStatus(page)
+        }))}
+      />
       <WikiDirectory pages={directoryPages} />
 
       <section className="recent-updates" aria-labelledby="recent-updates-title">
