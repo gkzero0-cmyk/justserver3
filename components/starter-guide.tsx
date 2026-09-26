7c22df2a35fdd05a0199b663e7ec24e369c14350
gkzero0-cmyk <gkzero0@gmail.com>
2026-09-26 13:26:33 +0900

    Fix starter guide route import

--- components/starter-guide.tsx
+++ components/starter-guide.tsx
@@ -8,7 +8,6 @@ import { isDraftPage } from '@/lib/wiki-content-status'
 import { readWikiStringArray } from '@/lib/wiki-client-state'
 import { withBasePath } from '@/lib/url-utils'
 import { wikiGuidePath } from '@/lib/wiki-routes'
-import { wikiGuidePath } from '@/lib/wiki-routes'
 
 const STEP_TITLES = ['서버규칙','기초설정(뉴비필독)','빚 갚기','채광','장비강화']
 
